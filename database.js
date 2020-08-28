956c02592c15adb43195f9864ae16127ea6bb961
vbedardl <bedard_v@hotmail.fr>
2020-08-27 20:48:15 -0400

    initial commit for the beer_app api

---
+++ database.js
@@ -0,0 +1,13 @@
+const db = require("./server");
+
+// const getBeerList = function () {
+//   return db
+//     .query(
+//       `
+//       SELECT ...
+//     `
+//     )
+//     .then((res) => res.rows)
+//     .catch((e) => null);
+// };
+// exports.getBeerList = getBeerList;
---
+++ server.js
@@ -0,0 +1,45 @@
+// load .env data into process.env
+require("dotenv").config();
+
+// Web server config
+const PORT = process.env.PORT || 5001;
+const ENV = process.env.ENV || "development";
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const bcrypt = require("bcrypt");
+const app = express();
+
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+
+module.exports = db;
+
+//const database = require("./database");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+    maxAge: 60 * 60 * 1000 * 1,
+  })
+);
+
+// Separated Routes for each Resource
+//const usersRoutes = require("./routes/users");
+// Mount all resource routes
+//app.use("/api/users", usersRoutes);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
