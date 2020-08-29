2d5860e2b8036cc8b9052493459c7162acff8bba
robpolisena <robpolisena@gmail.com>
2020-08-29 11:45:27 -0400

    cleaned registrations code

--- routes/registration.js
+++ routes/registration.js
@@ -14,38 +14,20 @@ const login = function (email, password) {
   });
 };
 
-// const generateToken = (res, id, firstname) => {
-//   const expiration = process.env.DB_ENV === 'testing' ? 100 : 604800000;
-//   const token = jwt.sign({ id, firstname }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.DB_ENV === 'testing' ? '1d' : '7d',
-//   });
-//   return res.cookie('token', token, {
-//     expires: new Date(Date.now() + expiration),
-//     secure: false, // set to true if your using https
-//     httpOnly: true,
-//   });
-// };
-
-
-
-
 //AUTHENTICATE MIDDLEWARE FOR THE JSONWEBTOKEN
 function authenticate(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  console.log(req.session.token);
+  const token = req.session.ACCESS_TOKEN_SECRET
   if (token == null) return res.sendStatus(401);
-
   jwt.verify(token.process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
-    next();
+  next();
   });
 }
 
 module.exports = () => {
-  router.get("/beers", (req, res) => {
-    database.getBeers().then((data) => res.send({ data }));
-  });
+  
   //LOGIN A USER
   router.post("/login", (req, res) => {
     const { email, password } = req.body;
@@ -56,6 +38,7 @@ module.exports = () => {
           res.send("didnt work");
         }
         const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+        req.session.token = accessToken
         res.json({ user, accessToken });
         res.status(200);
       })
@@ -83,6 +66,7 @@ module.exports = () => {
                 user,
                 process.env.ACCESS_TOKEN_SECRET
               );
+              req.session.token = accessToken
               res.json({ user, accessToken });
               res.status(200);
               //res.send(user)
@@ -97,13 +81,9 @@ module.exports = () => {
 
   //LOGOUT A USER
   router.post("/logout", (req, res) => {
-    const { token } = req.body;
-    //console.log(req.body);
-    //console.log(token);
-    //token = "deleted"
-    //console.log(token);
+    //const { token } = req.body;
+    req.session = null
     
-    //jwt.destroy(token);
     res.send("Logout successful");
   });
 
