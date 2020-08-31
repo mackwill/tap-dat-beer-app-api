cc13cf888d55884696d53d8d9a9d16265ee403a5
Will <mack.mindenhall@gmail.com>
2020-08-31 18:31:53 -0400

    Added authentication functions to recommendations

--- routes/beers.js
+++ routes/beers.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const database = require("../database");
+const jwt = require("jsonwebtoken");
+
+//AUTHENTICATE MIDDLEWARE FOR THE JSONWEBTOKEN
+function authenticate(req, res, next) {
+  console.log(req.session.token);
+  const token = req.session.token;
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+  });
+  next();
+}
 
 module.exports = () => {
   //GET ALL THE BEERS
@@ -11,7 +25,7 @@ module.exports = () => {
     database.getBeers().then((data) => res.send({ data }));
   });
 
-  router.get("/recommendations", (req, res) => {
+  router.get("/recommendations", authenticate, (req, res) => {
     const userId = req.session.userid;
     database
       .getRecommendationsForUser(userId)
