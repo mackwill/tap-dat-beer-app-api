9150106a21e5ba7a3a2456d4533653395a65fd9b
vbedardl <bedard_v@hotmail.fr>
2020-09-02 19:31:56 -0400

    db query 02

--- db/queries/02_queries.sql
+++ db/queries/02_queries.sql
@@ -3,7 +3,15 @@
       -- GROUP BY beers.id
       -- ORDER BY avg_rank DESC
 
-      -- SELECT beers.*, COUNT(reviews.*) as num_reviews FROM beers
-      -- LEFT JOIN reviews ON reviews.beer_id = beers.id
-      -- GROUP BY beers.id 
-      -- ORDER BY num_reviews DESC
+      SELECT beers.*, COUNT(reviews.*) as num_reviews FROM beers
+      LEFT JOIN reviews ON reviews.beer_id = beers.id
+      WHERE reviews.user_id NOT IN (3)
+      GROUP BY beers.id 
+      ORDER BY num_reviews DESC
+      LIMIT 10
+
+        SELECT beers.*, COUNT(reviews.*) as num_reviews FROM beers
+      LEFT JOIN reviews ON reviews.beer_id = beers.id
+      GROUP BY beers.id 
+      ORDER BY num_reviews DESC
+      LIMIT 10
